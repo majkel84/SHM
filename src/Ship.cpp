43d4131df105937fb29764b6a6e475e5abf3fbce
majkel84 <michal.sobotka.84@gmail.com>
2021-02-18 22:38:24 +0100

    SHM new Ship members and constructors

--- src/Ship.cpp
+++ src/Ship.cpp
@@ -1,7 +1,15 @@
 #include "Ship.hpp"
 
+Ship::Ship(size_t id, std::string& name, size_t speed, size_t maxCrew, size_t capacity)
+    : id_(id), name_(name), speed_(speed), maxCrew_(maxCrew), capacity_(capacity)
+    {}
+
+Ship::Ship(const std::string& name, size_t capacity, size_t maxCrew)
+    : name_(name), capacity_(capacity), maxCrew_(maxCrew)
+    {}
+
 Ship::Ship(const std::string& name, size_t capacity)
-    : name_(name), capacity_(capacity)
+    : Ship(name, capacity, 0)
     {}
 
 Ship::~Ship() {}
--- src/Ship.hpp
+++ src/Ship.hpp
@@ -4,10 +4,15 @@
 
 class Ship {
 public:
+    Ship(size_t id, std::string& name, size_t speed, size_t maxCrew, size_t capacity);
+    Ship(const std::string& name, size_t capacity, size_t maxCrew);
     Ship(const std::string& name, size_t capacity);
     ~Ship();
 
 private:
+    size_t id_;
     std::string name_;
+    size_t speed_;
+    size_t maxCrew_;
     const size_t capacity_;
 };
