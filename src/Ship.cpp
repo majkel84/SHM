08868a78d1a7131837a954a3e04ac7f63ee0f17c
majkel84 <michal.sobotka.84@gmail.com>
2021-02-18 22:56:11 +0100

    Ship getters and setters

--- src/Ship.cpp
+++ src/Ship.cpp
@@ -5,6 +5,7 @@ Ship::Ship(size_t id, std::string& name, size_t speed, size_t maxCrew, size_t ca
     {}
 
 Ship::Ship(const std::string& name, size_t capacity, size_t maxCrew)
+    //: Ship(0, name, 0, maxCrew, capacity)
     : name_(name), capacity_(capacity), maxCrew_(maxCrew)
     {}
 
@@ -13,3 +14,27 @@ Ship::Ship(const std::string& name, size_t capacity)
     {}
 
 Ship::~Ship() {}
+
+size_t Ship::getId() {
+    return id_;
+}
+
+std::string Ship::getName() {
+    return name_;
+}
+
+size_t Ship::getSpeed() {
+    return speed_;
+}
+
+void Ship::setSpeed(size_t newSpeed) {
+    speed_ = newSpeed;
+}
+
+size_t Ship::getCrew() {
+    return maxCrew_;
+}
+
+size_t Ship::getCapacity() {
+    return capacity_;
+}
--- src/Ship.hpp
+++ src/Ship.hpp
@@ -9,10 +9,17 @@ public:
     Ship(const std::string& name, size_t capacity);
     ~Ship();
 
+    size_t getId();
+    std::string getName();
+    size_t getSpeed();
+    void setSpeed(size_t newSpeed);
+    size_t getCrew();
+    size_t getCapacity();
+
 private:
     size_t id_;
-    std::string name_;
+    const std::string name_;
     size_t speed_;
-    size_t maxCrew_;
+    const size_t maxCrew_;
     const size_t capacity_;
 };
