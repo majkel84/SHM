7b31efe0fbae8d8f1e5dfff1c80baae1640a0702
majkel84 <michal.sobotka.84@gmail.com>
2021-02-18 23:12:45 +0100

    Ship constructors corrected

--- src/Ship.cpp
+++ src/Ship.cpp
@@ -4,15 +4,6 @@ Ship::Ship(size_t id, std::string& name, size_t speed, size_t maxCrew, size_t ca
     : id_(id), name_(name), speed_(speed), maxCrew_(maxCrew), capacity_(capacity)
     {}
 
-Ship::Ship(const std::string& name, size_t capacity, size_t maxCrew)
-    //: Ship(0, name, 0, maxCrew, capacity)
-    : name_(name), capacity_(capacity), maxCrew_(maxCrew)
-    {}
-
-Ship::Ship(const std::string& name, size_t capacity)
-    : Ship(name, capacity, 0)
-    {}
-
 Ship::~Ship() {}
 
 size_t Ship::getId() {
--- src/Ship.hpp
+++ src/Ship.hpp
@@ -5,8 +5,6 @@
 class Ship {
 public:
     Ship(size_t id, std::string& name, size_t speed, size_t maxCrew, size_t capacity);
-    Ship(const std::string& name, size_t capacity, size_t maxCrew);
-    Ship(const std::string& name, size_t capacity);
     ~Ship();
 
     size_t getId();
@@ -18,8 +16,8 @@ public:
 
 private:
     size_t id_;
-    const std::string name_;
+    std::string name_;
     size_t speed_;
-    const size_t maxCrew_;
-    const size_t capacity_;
+    size_t maxCrew_;
+    size_t capacity_;
 };
